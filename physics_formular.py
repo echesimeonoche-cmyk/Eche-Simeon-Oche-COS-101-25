0b4d84d4a2546d125b639751ff520344314e1165
echesimeonoche-cmyk <echesimeonoche@gmail.com>
2025-12-03 00:43:49 +0100

    Delete physics_formular.py

--- physics_formular.py
+++
@@ -1,8 +0,0 @@
-age = int(input("enter your age"))
-height = float(input("enter your height"))
-weight = 70
-
-if age >= 18 or height >= 1.7:
-    print("you are eligible")
-else:
-    print("you are not eligible")
